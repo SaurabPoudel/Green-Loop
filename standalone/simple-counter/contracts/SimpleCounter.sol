142d516a4562adb6d147b39270a7e6b167096080
SaurabPoudel <poudelsaurab20@gmail.com>
2024-06-30 18:28:16 +0545

    simple counter deployed

---
+++ standalone/simple-counter/contracts/SimpleCounter.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract SimpleCounter {
+    uint256 public number;
+
+    constructor(uint256 _initialNumber) {
+        number = _initialNumber;
+    }
+
+    event NumberIncremented(uint _updatedNumber);
+    event NumberDecremented(uint _updatedNumber);
+    function increment() public {
+        number += 1;
+        emit NumberDecremented(number);
+    }
+
+    function decrement() public {
+        number -= 1;
+        emit NumberDecremented(number);
+    }
+}
